9db076fdd7649e64bc40785e2170a3217da471e2
Michael Ellis <michael94ellis@gmail.com>
2020-04-06 16:16:18 -0400

    Initial Commit

---
+++ WorldMakerTests/WorldMakerTests.swift
@@ -0,0 +1,34 @@
+//
+//  WorldMakerTests.swift
+//  WorldMakerTests
+//
+//  Created by Michael Ellis on 4/6/20.
+//  Copyright © 2020 Mellis. All rights reserved.
+//
+
+import XCTest
+@testable import WorldMaker
+
+class WorldMakerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
